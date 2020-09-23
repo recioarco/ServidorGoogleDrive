9b74043af6659a94e9c87503e26ad28089dbd424
Antonio Recio Arco <antoniorecioarco@MacBook-Pro-de-Antonio.local>
2020-09-23 17:44:25 +0200

    Creación de front con React

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Element extends React.Component {
+  
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Directory extends React.Component {
+    render() {
+      return (
+     <Container>
+        <Row>
+          <Col>HOLA K ASES</Col>
+          <Col>JAJAJJA</Col>
+        </Row>
+        <Row>
+          <Card>
+            <Card.ImgOverlay src="holder.js/100px180" />
+            <Card.Body>This is some text within a card body.</Card.Body>
+          </Card> 
+        </Row>
+      </Container>
+      );
+    }
+  }
+
+ // ==========================================
+  
+ ReactDOM.render(
+    <Directory />,
+    document.getElementById('root')
+  );
